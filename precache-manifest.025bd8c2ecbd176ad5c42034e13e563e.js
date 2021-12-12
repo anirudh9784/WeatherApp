bba760b2af276db4911f8c3d13c36e9799a2960a
Anirudh Yadav <anirudhyadav@Anirudhs-MacBook-Air.local>
2021-12-12 18:28:17 +0530

    Updates

---
+++ precache-manifest.025bd8c2ecbd176ad5c42034e13e563e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce559b3a0a2531fedf0f7d7ec28d2d43",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "42979e5fb02cdceff4c1",
+    "url": "/WeatherApp/static/css/main.0af3b52f.chunk.css"
+  },
+  {
+    "revision": "16bee0f1faf6d292f754",
+    "url": "/WeatherApp/static/js/2.ab69dbd2.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/WeatherApp/static/js/2.ab69dbd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf075a10f81b6223330",
+    "url": "/WeatherApp/static/js/3.1ac1e04f.chunk.js"
+  },
+  {
+    "revision": "42979e5fb02cdceff4c1",
+    "url": "/WeatherApp/static/js/main.da25fcb4.chunk.js"
+  },
+  {
+    "revision": "14f6484376acab5dac82",
+    "url": "/WeatherApp/static/js/runtime-main.fca64e2f.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/WeatherApp/static/media/cold-bg.c97cb84b.jpeg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/WeatherApp/static/media/warm-bg.635ed87d.jpeg"
+  }
+]);
